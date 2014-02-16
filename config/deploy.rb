48bb650883a6991439685b418a4043e10a88b711
Jingkai He <jaxihe@gmail.com>
2014-02-16 21:15:53 +0000

    ready to deploy

---
+++ config/deploy.rb
@@ -0,0 +1,57 @@
+require "bundler/capistrano"
+require "dotenv/capistrano"
+
+server "jaxihjk.me", :web, :app, :db, primary: true
+
+set :application, "hack"
+set :user, "jaxi"
+set :deploy_to, "/home/#{user}/web_apps/#{application}"
+set :deploy_via, :remote_cache
+set :use_sudo, false
+
+set :scm, "git"
+set :repository, "git@github.com:jaxi/#{application}.git"
+set :branch, "master"
+
+default_run_options[:pty] = true
+ssh_options[:forward_agent] = true
+
+after "deploy", "deploy:cleanup" # keep only the last 5 releases
+
+namespace :deploy do
+  %w[start stop restart].each do |command|
+    desc "#{command} unicorn server"
+    task command, roles: :app, except: {no_release: true} do
+      run "/etc/init.d/unicorn_#{application} #{command}"
+    end
+  end
+  task :setup_nginx_config, roles: :app do
+    sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-available/#{application}"
+    sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
+    sudo "ln -nfs #{current_path}/config/unicorn_init.sh /etc/init.d/unicorn_#{application}"
+  end
+
+  task :setup_shared_config, roles: :app do
+    run "mkdir -p #{shared_path}/config"
+    put File.read("config/database.example.yml"), "#{shared_path}/config/database.yml"
+    put File.read(".sample.env"), "#{shared_path}/.env"
+    puts "Now edit the config files in #{shared_path}."
+  end
+
+  after "deploy:setup", "deploy:setup_nginx_config", "deploy:setup_shared_config"
+
+  task :symlink_config, roles: :app do
+    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+  end
+  after "deploy:finalize_update", "deploy:symlink_config"
+
+  desc "Make sure local git is in sync with remote."
+  task :check_revision, roles: :web do
+    unless `git rev-parse HEAD` == `git rev-parse origin/master`
+      puts "WARNING: HEAD is not the same as origin/master"
+      puts "Run `git push` to sync changes."
+      exit
+    end
+  end
+  before "deploy", "deploy:check_revision"
+end
