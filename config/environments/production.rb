3b7d8616d4b0c0fb348fa354f2bff76e2c82f0fd
Jingkai He <jaxihe@gmail.com>
2014-02-17 22:16:06 +0000

    remove useless precompile, speedup deployment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,6 @@ Hack::Application.configure do
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
   # config.assets.precompile += %w( search.js )
-  config.assets.precompile += %w(lumen.js lumen.css)
 
   # Adding Webfonts to the Asset Pipeline
   config.assets.precompile << Proc.new { |path|
