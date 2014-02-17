eb11b6a0ef70dee089e35fece0307c3254e78dce
Jingkai He <jaxihe@gmail.com>
2014-02-17 15:05:11 +0000

    login, logout and oauth completed

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Hack::Application.routes.draw do
   mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
   get "pages/index"
   root 'pages#index'
+
+  get 'auth/:provider/callback', to: 'session#create'
+  get 'auth/failure', to: redirect('/')
+  delete 'signout', to: 'session#destroy', as: 'signout'
 end
