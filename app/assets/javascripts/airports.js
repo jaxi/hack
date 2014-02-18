93a1161015325b28d862027b9385b551578fb8f1
Jingkai He <jaxihe@gmail.com>
2014-02-18 15:06:31 +0000

    dirty fix...

--- app/assets/javascripts/airports.js
+++ app/assets/javascripts/airports.js
@@ -3,7 +3,7 @@ $(function(){
   $(".search-query").typeahead({
     source: function(query, process){
       return $.get('/airports/autocomplete',
-        {query: $(".search-query").val()}, function(data){
+        {query: query}, function(data){
           result = $.map( data, function( val, i ) {
             return val.airport_name + " Airport, " + val.city_name;
           });
